2a0d1e6c070d66301cb3fc7a69a65de854bb14a7
Rodney Payne <paynerg2@gmail.com>
2019-05-10 14:44:57 -0400

    Remove deprecated Mongoose option

--- src/server/_helpers/db.js
+++ src/server/_helpers/db.js
@@ -2,7 +2,8 @@ const config = require('../config.json');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, {
     useCreateIndex: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 // this may not be necessary with Mongoose 5
 mongoose.Promise = global.Promise;
