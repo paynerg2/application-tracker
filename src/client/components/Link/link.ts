b1ba2e5bfb9b359854434680120d5a70b94892e2
Rodney Payne <paynerg2@gmail.com>
2021-11-08 15:40:47 -0500

    Extracted reusable styles
    - Created Link component
    - Created individual form components

---
+++ src/client/components/Link/link.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Link as RouterLink } from 'react-router-dom';
+
+export const Link = styled(RouterLink)`
+    color: ${(props) => props.theme.color.link};
+    text-decoration: none;
+    font-weight: 500;
+`;
+
+export default Link;
