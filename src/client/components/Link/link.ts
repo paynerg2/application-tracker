eea4cad470a8b1ef336973e28242bdf915a2b7b3
Rodney Payne <paynerg2@gmail.com>
2021-11-16 14:55:07 -0500

    Display link as block element

--- src/client/components/Link/link.ts
+++ src/client/components/Link/link.ts
@@ -5,6 +5,7 @@ export const Link = styled(RouterLink)`
     color: ${(props) => props.theme.color.link};
     text-decoration: none;
     font-weight: 500;
+    display: block;
 `;
 
 export default Link;
