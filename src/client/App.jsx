f7946867a56a851179c879edacb32bef2ef41aee
Rodney Payne <paynerg2@gmail.com>
2021-11-18 11:34:39 -0500

    Add basic routing

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
 import { useGetApplicationsQuery } from './services/api';
 import mobileBackground from './assets/Background_mobile.png';
 import desktopBackground from './assets/Background_desktop.png';
 import Login from './pages/Login/Login';
 import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
+import Signup from './pages/Signup/Signup';
 
 const Container = styled.div`
     min-height: 100vh;
@@ -18,15 +21,19 @@ const Container = styled.div`
 `;
 
 function App() {
-    const { data, error, isLoading } = useGetApplicationsQuery();
+    // const { data, error, isLoading } = useGetApplicationsQuery();
 
-    if (isLoading) return <div>Loading...</div>;
+    // if (isLoading) return <div>Loading...</div>;
 
     return (
         <>
             <Container>
                 <Header />
-                <Login />
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                </Switch>
+                <Footer />
             </Container>
         </>
     );
--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -2,7 +2,6 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import { api } from '../services/api';
 import { authApi } from '../services/authApi';
-import signupReducer from '../features/forms/signupSlice';
 import authReducer from '../features/auth/authSlice';
 
 const loggerMiddleware = createLogger();
@@ -13,7 +12,6 @@ export const store = configureStore({
         [api.reducerPath]: api.reducer,
         auth: authReducer,
         [authApi.reducerPath]: authApi.reducer,
-        signupForm: signupReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
 });
