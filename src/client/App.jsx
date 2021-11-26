7f14ef6126d4ae643dd5f4ddf777e420e7a739fe
Rodney Payne <paynerg2@gmail.com>
2021-11-26 10:49:08 -0500

    Add 404 not found page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { useGetApplicationsQuery } from './services/api';
 import mobileBackground from './assets/Background_mobile.png';
 import desktopBackground from './assets/Background_desktop.png';
@@ -8,12 +8,18 @@ import Login from './pages/Login/Login';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import Signup from './pages/Signup/Signup';
+import ContactCard from './components/Cards/Contact/contactCard';
+import InterviewCard from './components/Cards/Interview/interviewCard';
+import ApplicationItem from './components/Cards/Application/applicationItem';
+import { List } from './components/List/list';
+import NotFound from './pages/404/404';
 
 const Container = styled.div`
     min-height: 100vh;
-    min-width: 100vw;
+    min-width: 100%;
     background-image: url(${mobileBackground});
     background-size: cover;
+    box-sizing: border-box;
 
     @media (min-width: ${(props) => props.theme.breakpoint.mobile}) {
         background-image: url(${desktopBackground});
@@ -32,6 +38,8 @@ function App() {
                 <Switch>
                     <Route path="/login" component={Login} />
                     <Route path="/signup" component={Signup} />
+                    <Route path="/404" component={NotFound} />
+                    <Redirect to="/404" />
                 </Switch>
                 <Footer />
             </Container>
---
+++ src/client/pages/404/404.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import NotFoundImage from '../../assets/Not_Found.svg';
+import Button from '../../components/Button/button';
+
+const Background = styled.div`
+    height: 90vh;
+    width: 100%;
+    background: linear-gradient(
+        to bottom,
+        transparent 40%,
+        ${(props) => props.theme.color.primaryBlue} 40%
+    );
+
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    gap: 8vh;
+`;
+
+const Text = styled.p`
+    font-size: 2rem;
+    color: ${(props) => props.theme.color.white};
+    text-align: center;
+`;
+
+function NotFound() {
+    const history = useHistory();
+    return (
+        <Background>
+            <img
+                style={{ height: 'auto', width: '100%', maxWidth: '700px' }}
+                src={NotFoundImage}
+                alt="Page Not Found"
+            />
+            <Text>
+                <strong>We can't find the content you were looking for.</strong>
+            </Text>
+            <Link to="/" style={{ width: '50vw' }}>
+                <Button style={{ maxWidth: '400px', margin: '0 auto' }} inverted={true}>
+                    Get Back to Your Job Hunt
+                </Button>
+            </Link>
+        </Background>
+    );
+}
+
+export default NotFound;
