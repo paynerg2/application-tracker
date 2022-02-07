8a52de606e7c7bfb5084cf7a0298fb660676dde6
Rodney Payne <paynerg2@gmail.com>
2022-02-07 15:46:06 -0500

    Add /applications routes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { useGetApplicationsQuery } from './services/api';
 import mobileBackground from './assets/Background_mobile.png';
 import desktopBackground from './assets/BG.png';
 import ScrollToTop from './common/ScrollToTop/scrollToTop';
 import Login from './pages/Login/Login';
+import Applications from './pages/Applications/Applications';
+import Application from './pages/Applications/Application';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import Signup from './pages/Signup/Signup';
-import ContactCard from './components/Cards/Contact/contactCard';
-import InterviewCard from './components/Cards/Interview/interviewCard';
-import ApplicationItem from './components/Cards/Application/applicationItem';
-import { List } from './components/List/list';
 import NotFound from './pages/404/404';
 import LandingPage from './pages/Landing/Landing';
+import NewApplication from './pages/NewApplication/NewApplication';
 import Test from './pages/Test/Test';
 
 const Container = styled.div`
@@ -45,6 +43,9 @@ function App() {
                         <Route exact path="/" component={LandingPage} />
                         <Route path="/login" component={Login} />
                         <Route path="/signup" component={Signup} />
+                        <Route exact path="/applications" component={Applications} />
+                        <Route path="/applications/new/:step" component={NewApplication} />
+                        <Route exact path="/applications/:id" component={Application} />
                         <Route path="/404" component={NotFound} />
                         <Route path="/testing" component={Test} />
                         <Redirect to="/404" />
