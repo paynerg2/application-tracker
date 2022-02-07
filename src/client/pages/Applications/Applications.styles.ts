ddceb3df82ff32afe097881f6d353ba345376d90
Rodney Payne <paynerg2@gmail.com>
2022-02-07 15:50:09 -0500

    Add applications list page

---
+++ src/client/pages/Applications/Applications.styles.ts
@@ -0,0 +1,131 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+    display: grid;
+
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: repeat(15vh);
+
+    row-gap: 2vh;
+
+    width: 80vw;
+    margin: 0 auto;
+`;
+
+export const NewApplication = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    justify-self: flex-start;
+
+    grid-column: 3 / 6;
+    grid-row: 1 /2;
+
+    color: ${(props) => props.theme.color.white};
+    background: ${(props) => props.theme.color.primaryBlue};
+    border-radius: ${(props) => props.theme.borders.radius};
+    width: 80%;
+
+    a {
+        width: 60%;
+        margin: 3vh 0;
+    }
+
+    p,
+    h2 {
+        margin: 0;
+    }
+    h2 {
+        font-size: 2rem;
+        margin-top: 3vh;
+    }
+`;
+
+export const SubmissionsList = styled.div`
+    grid-column: 3 / 7;
+    grid-row: 2 / 3;
+`;
+
+export const SubmissionsContainer = styled.div`
+    display: grid;
+    grid-template-columns: 15% 1fr 1fr;
+
+    height: fit-content;
+    width: 100%;
+
+    background: ${(props) => props.theme.color.veryLightGray};
+    border-radius: ${(props) => props.theme.borders.radius};
+    margin-bottom: 1vh;
+
+    padding: 0.8em;
+`;
+export const ApplicationCardContainer = styled.div`
+    grid-column: 2 / 4;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    gap: 2vmin;
+`;
+
+export const ApplicationListContainer = styled.ul`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    min-height: 20vmin;
+    height: fit-content;
+    width: 100%;
+    background: linear-gradient(
+        to right,
+        ${(props) => props.theme.color.primaryBlue} 10%,
+        ${(props) => props.theme.color.lightBlue} 10%,
+        ${(props) => props.theme.color.lightBlue} 100%
+    );
+    list-style: none;
+
+    border-radius: ${(props) => props.theme.borders.radius};
+
+    gap: 2vmin;
+    padding: 2vmin 0;
+    box-sizing: border-box;
+`;
+
+export const ApplicationListItem = styled.li`
+    display: grid;
+    // This syntax gives 5 equal-sized columns
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+    grid-auto-flow: column;
+    align-items: center;
+
+    height: 15vmin;
+    width: 95%;
+
+    border-radius: ${(props) => props.theme.borders.radius};
+
+    background-color: ${(props) => props.theme.color.white};
+
+    cursor: pointer;
+
+    > div {
+        max-width: 100%;
+        width: 100%;
+        height: 90%;
+        border-right: solid 2px ${(props) => props.theme.color.veryLightGray};
+        word-wrap: break-word;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        padding: 2vmin;
+        box-sizing: border-box;
+        text-align: center;
+        font-size: 1.5em;
+    }
+`;
---
+++ src/client/pages/Applications/Applications.tsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Button from '../../components/Button/button';
+import {
+    ApplicationCardContainer,
+    ApplicationListContainer,
+    ApplicationListItem,
+    Layout,
+    NewApplication,
+    SubmissionsContainer,
+    SubmissionsList,
+} from './Applications.styles';
+import DateContainer from '../../components/DateContainer/dateContainer';
+
+import { Application } from '../../interfaces/application';
+import ApplicationCard from '../../components/Cards/Application/applicationCard';
+import ApplicationFilter from '../../components/Cards/Application/ApplicationFilter';
+import { applicationHelpers } from '../../_helpers/applicationHelpers';
+import { useGetApplicationsQuery } from '../../services/api';
+import { iconSelector } from '../../_helpers/iconSelector';
+
+function Applications() {
+    // todo: Replace this with hooks instead of passing callbacks
+    const [isCardView, setIsCardView] = useState(true);
+    const [filters, setFilters] = useState<string[]>([]);
+    const { data, error, isLoading } = useGetApplicationsQuery();
+    const history = useHistory();
+
+    // Todo: Add a Skeleton (suspense?)
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    // Todo: Add a better error dialogue
+    if (error || !data) {
+        return <div>Something went wrong...</div>;
+    }
+
+    // Need to act on copies of data, because sorting directly affects the object.
+    const groupedApplications = applicationHelpers.groupApplicationsByDate(
+        applicationHelpers.filterApplications(data, filters)
+    );
+    const applications = [...data];
+
+    const onFilterChange = (newFilter: string) => {
+        if (filters.includes(newFilter)) {
+            // remove
+            setFilters((prev) => prev.filter((f) => f !== newFilter));
+        } else {
+            // add
+            setFilters((prev) => prev.concat(newFilter));
+        }
+    };
+
+    if (error) {
+        return <div>Error</div>;
+    }
+
+    const getCardView = () => {
+        return (
+            <>
+                {!isLoading ? (
+                    <SubmissionsList>
+                        <h2 style={{ fontSize: '2em' }}>
+                            <strong>Submitted Applications</strong>
+                        </h2>
+                        {/* Map submissions to an application card separated by
+                submission date */}
+                        {groupedApplications ? (
+                            Object.keys(groupedApplications)
+                                .sort((a, b) => {
+                                    const aDate = new Date(a);
+                                    const bDate = new Date(b);
+                                    // Sort in descending order (i.e. newest date first)
+                                    return bDate.getTime() - aDate.getTime();
+                                })
+                                .map((date) => (
+                                    <SubmissionsContainer key={date}>
+                                        <DateContainer date={date} />
+                                        <ApplicationCardContainer>
+                                            {groupedApplications[date].map((app: Application) => (
+                                                <ApplicationCard key={app.id} application={app} />
+                                            ))}
+                                        </ApplicationCardContainer>
+                                    </SubmissionsContainer>
+                                ))
+                        ) : (
+                            <div>Try adding some applications!</div>
+                        )}
+                    </SubmissionsList>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </>
+        );
+    };
+
+    const getListView = () => {
+        return (
+            <>
+                {!isLoading ? (
+                    <SubmissionsList>
+                        <h2 style={{ fontSize: '2em' }}>
+                            <strong>Submitted Applications</strong>
+                        </h2>
+                        <ApplicationListContainer>
+                            {applications
+                                .sort((a, b) => {
+                                    const aDate = new Date(a.dateApplicationSent);
+                                    const bDate = new Date(b.dateApplicationSent);
+                                    // Sort in descending order (i.e. newest date first)
+                                    return bDate.getTime() - aDate.getTime();
+                                })
+                                .map((application) => (
+                                    <ApplicationListItem
+                                        key={application.id}
+                                        onClick={() =>
+                                            history.push(`/applications/${application.id}`)
+                                        }
+                                    >
+                                        <DateContainer date={application.dateApplicationSent} />
+                                        <div>
+                                            <strong>{application.jobTitle}</strong>
+                                        </div>
+                                        <div>
+                                            <strong>{application.company}</strong>
+                                        </div>
+                                        <div>{application.location}</div>
+                                        <div>{iconSelector(application.mainSkill)}</div>
+                                    </ApplicationListItem>
+                                ))}
+                        </ApplicationListContainer>
+                    </SubmissionsList>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </>
+        );
+    };
+
+    return (
+        <Layout>
+            <NewApplication>
+                <h2>Add Application</h2>
+                <p>Log a new submitted application</p>
+                <Link to="/applications/new/1">
+                    <Button inverted>New Application</Button>
+                </Link>
+            </NewApplication>
+            {isCardView ? getCardView() : getListView()}
+            <ApplicationFilter
+                applications={data || []}
+                filters={filters}
+                onChange={onFilterChange}
+                setIsCardView={setIsCardView}
+            />
+        </Layout>
+    );
+}
+
+export default Applications;
