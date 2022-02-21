17ab254aa2a73610e42983e79d20f7feacb59531
Rodney Payne <paynerg2@gmail.com>
2022-02-20 21:11:57 -0500

    Add queries/mutations for Contacts & Interviews

--- src/client/services/api.ts
+++ src/client/services/api.ts
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Application } from '../interfaces/application';
 import config from '../config.json';
 import { Interview } from '../interfaces/interviews';
+import { Contact } from '../interfaces/contact';
 
 export const api = createApi({
     reducerPath: 'api',
@@ -50,9 +51,59 @@ export const api = createApi({
             }),
             invalidatesTags: ['Applications'],
         }),
+        getContacts: build.query<Contact[], void>({
+            query: () => 'contacts',
+            providesTags: ['Contacts'],
+        }),
+        addNewContact: build.mutation<Contact, Partial<Contact>>({
+            query: (initialContact) => ({
+                url: '/contacts',
+                method: 'POST',
+                body: initialContact,
+            }),
+            invalidatesTags: ['Contacts'],
+        }),
+        editContact: build.mutation<Contact, Partial<Contact>>({
+            query: (contact) => ({
+                url: `/contacts/${contact.id}`,
+                method: 'PUT',
+                body: contact,
+            }),
+            invalidatesTags: ['Contacts'],
+        }),
+        deleteContact: build.mutation<void, string>({
+            query: (id) => ({
+                url: `/contacts/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Contacts'],
+        }),
         getInterviews: build.query<Interview[], void>({
             query: () => 'interviews',
             providesTags: ['Interviews'],
+        }),
+        addNewInterview: build.mutation<Interview, Partial<Interview>>({
+            query: (initialInterview) => ({
+                url: '/interviews',
+                method: 'POST',
+                body: initialInterview,
+            }),
+            invalidatesTags: ['Interviews'],
+        }),
+        editInterview: build.mutation<Interview, Partial<Interview>>({
+            query: (interview) => ({
+                url: `/interviews/${interview.id}`,
+                method: 'PUT',
+                body: interview,
+            }),
+            invalidatesTags: ['Interviews'],
+        }),
+        deleteInterview: build.mutation<void, string>({
+            query: (id) => ({
+                url: `/interviews/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Interviews'],
         }),
     }),
 });
@@ -62,5 +113,12 @@ export const {
     useAddNewApplicationMutation,
     useEditApplicationMutation,
     useDeleteApplicationMutation,
+    useGetContactsQuery,
+    useAddNewContactMutation,
+    useEditContactMutation,
+    useDeleteContactMutation,
     useGetInterviewsQuery,
+    useAddNewInterviewMutation,
+    useEditInterviewMutation,
+    useDeleteInterviewMutation,
 } = api;
