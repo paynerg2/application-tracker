8ac9fad5df60795d48cd3fb9ed4c710d68ed9be3
Rodney Payne <paynerg2@gmail.com>
2022-03-01 22:50:08 -0500

    Remove unnecessary code

--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -2,7 +2,6 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import { api } from '../services/api';
 import { authApi } from '../services/authApi';
-import authReducer from '../features/auth/authSlice';
 
 const loggerMiddleware = createLogger();
 
@@ -10,7 +9,6 @@ export const middlewares = [loggerMiddleware, authApi.middleware, api.middleware
 export const store = configureStore({
     reducer: {
         [api.reducerPath]: api.reducer,
-        auth: authReducer,
         [authApi.reducerPath]: authApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
