fbd6dd5dafeccdc232231aef9f1be62fa6644fe9
Rodney Payne <paynerg2@gmail.com>
2022-03-14 15:39:42 -0400

    (Bug fix) Increase bodyParse json size limit.
    Increased to 50mb to avoid 'entity too large' error when sending
    Base64 image data to upload to Cloudinary.

--- src/server/server.js
+++ src/server/server.js
@@ -9,10 +9,8 @@ const bodyParser = require('body-parser');
 const jwt = require('./_helpers/jwt');
 const errorHandler = require('./_helpers/error-handler');
 
-console.log(process.env.API_KEY);
-
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
 app.use(jwt());
