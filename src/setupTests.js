8837b093aeef990dac1cafa1350022c9a8283f0c
Rodney Payne <paynerg2@gmail.com>
2022-03-28 14:56:10 -0400

    Add testing utilities

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,5 +4,5 @@ import EnzymeAdapter from 'enzyme-adapter-react-16';
 // Apply React 16 Enzyme adapter
 Enzyme.configure({
     adapter: new EnzymeAdapter(),
-    disableLifecycleMethods: true
+    disableLifecycleMethods: true,
 });
