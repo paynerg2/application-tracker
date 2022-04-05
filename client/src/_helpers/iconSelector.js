1b648cc22801d1fac2f995e7e8c938af50f1383b
Rodney Payne <paynerg2@gmail.com>
2022-04-05 11:45:22 -0400

    Update supported language icons

--- client/src/_helpers/iconSelector.js
+++ client/src/_helpers/iconSelector.js
@@ -1,47 +1,111 @@
 import React from 'react';
 
+import { ReactComponent as AdnoisJSIcon } from 'devicon/icons/adonisjs/adonisjs-original.svg';
 import { ReactComponent as AWSIcon } from 'devicon/icons/amazonwebservices/amazonwebservices-original.svg';
 import { ReactComponent as AndroidIcon } from 'devicon/icons/android/android-original.svg';
 import { ReactComponent as AngularJSIcon } from 'devicon/icons/angularjs/angularjs-original.svg';
+import { ReactComponent as ArduinoIcon } from 'devicon/icons/arduino/arduino-original.svg';
+import { ReactComponent as AzureIcon } from 'devicon/icons/azure/azure-original.svg';
+import { ReactComponent as BackboneIcon } from 'devicon/icons/backbonejs/backbonejs-original.svg';
 import { ReactComponent as BootstrapIcon } from 'devicon/icons/bootstrap/bootstrap-plain.svg';
+import { ReactComponent as CoffeescriptIcon } from 'devicon/icons/coffeescript/coffeescript-original.svg';
+import { ReactComponent as ClojureIcon } from 'devicon/icons/clojure/clojure-original.svg';
 import { ReactComponent as CIcon } from 'devicon/icons/c/c-original.svg';
 import { ReactComponent as CPlusPlusIcon } from 'devicon/icons/cplusplus/cplusplus-original.svg';
+import { ReactComponent as CouchDBIcon } from 'devicon/icons/couchdb/couchdb-original.svg';
 import { ReactComponent as CsharpIcon } from 'devicon/icons/csharp/csharp-original.svg';
 import { ReactComponent as Css3Icon } from 'devicon/icons/css3/css3-original.svg';
 import { ReactComponent as D3JsIcon } from 'devicon/icons/d3js/d3js-plain.svg';
+import { ReactComponent as DartIcon } from 'devicon/icons/dart/dart-original.svg';
 import { ReactComponent as DjangoIcon } from 'devicon/icons/django/django-plain.svg';
 import { ReactComponent as DockerIcon } from 'devicon/icons/docker/docker-original.svg';
 import { ReactComponent as DotNetIcon } from 'devicon/icons/dot-net/dot-net-original.svg';
+import { ReactComponent as DotNetCoreIcon } from 'devicon/icons/dotnetcore/dotnetcore-original.svg';
+import { ReactComponent as DrupalIcon } from 'devicon/icons/drupal/drupal-original.svg';
 import { ReactComponent as ElectronIcon } from 'devicon/icons/electron/electron-original.svg';
+import { ReactComponent as EmberIcon } from 'devicon/icons/ember/ember-original-wordmark.svg';
+import { ReactComponent as ErlangIcon } from 'devicon/icons/erlang/erlang-original.svg';
 import { ReactComponent as ExpressIcon } from 'devicon/icons/express/express-original.svg';
+import { ReactComponent as FigmaIcon } from 'devicon/icons/figma/figma-original.svg';
+import { ReactComponent as FirebaseIcon } from 'devicon/icons/firebase/firebase-plain.svg';
+import { ReactComponent as FlaskIcon } from 'devicon/icons/flask/flask-original.svg';
+import { ReactComponent as FlutterIcon } from 'devicon/icons/flutter/flutter-original.svg';
+import { ReactComponent as FSharpIcon } from 'devicon/icons/fsharp/fsharp-original.svg';
+import { ReactComponent as GatsbyIcon } from 'devicon/icons/gatsby/gatsby-original.svg';
+import { ReactComponent as GoIcon } from 'devicon/icons/go/go-original.svg';
+import { ReactComponent as GraphQLIcon } from 'devicon/icons/graphql/graphql-plain.svg';
+import { ReactComponent as HaskellIcon } from 'devicon/icons/haskell/haskell-original.svg';
 import { ReactComponent as Html5Icon } from 'devicon/icons/html5/html5-original.svg';
+import { ReactComponent as JamstackIcon } from 'devicon/icons/jamstack/jamstack-original.svg';
 import { ReactComponent as JavaIcon } from 'devicon/icons/java/java-original.svg';
 import { ReactComponent as JavascriptIcon } from 'devicon/icons/javascript/javascript-plain.svg';
 import { ReactComponent as JQueryIcon } from 'devicon/icons/jquery/jquery-original.svg';
+import { ReactComponent as JuliaIcon } from 'devicon/icons/julia/julia-original.svg';
+import { ReactComponent as KnockoutIcon } from 'devicon/icons/knockout/knockout-plain-wordmark.svg';
+import { ReactComponent as KotlinIcon } from 'devicon/icons/kotlin/kotlin-original.svg';
+import { ReactComponent as KubernetesIcon } from 'devicon/icons/kubernetes/kubernetes-plain.svg';
+import { ReactComponent as LabViewIcon } from 'devicon/icons/labview/labview-original.svg';
+import { ReactComponent as LaravelIcon } from 'devicon/icons/laravel/laravel-plain.svg';
+import { ReactComponent as LuaIcon } from 'devicon/icons/lua/lua-original.svg';
+import { ReactComponent as MagentoIcon } from 'devicon/icons/magento/magento-original.svg';
 import { ReactComponent as MongoDBIcon } from 'devicon/icons/mongodb/mongodb-original.svg';
 import { ReactComponent as MySQLIcon } from 'devicon/icons/mysql/mysql-original.svg';
+import { ReactComponent as NestJSIcon } from 'devicon/icons/nestjs/nestjs-plain.svg';
+import { ReactComponent as NextJSIcon } from 'devicon/icons/nextjs/nextjs-original.svg';
 import { ReactComponent as NodeJSIcon } from 'devicon/icons/nodejs/nodejs-original.svg';
+import { ReactComponent as NumpyIcon } from 'devicon/icons/numpy/numpy-original.svg';
+import { ReactComponent as ObjectiveCIcon } from 'devicon/icons/objectivec/objectivec-plain.svg';
+import { ReactComponent as PandasIcon } from 'devicon/icons/pandas/pandas-original.svg';
+import { ReactComponent as PerlIcon } from 'devicon/icons/perl/perl-original.svg';
+import { ReactComponent as PHPIcon } from 'devicon/icons/php/php-original.svg';
+import { ReactComponent as PostgresIcon } from 'devicon/icons/postgresql/postgresql-original.svg';
 import { ReactComponent as PythonIcon } from 'devicon/icons/python/python-original.svg';
+import { ReactComponent as RIcon } from 'devicon/icons/r/r-original.svg';
+import { ReactComponent as RailsIcon } from 'devicon/icons/rails/rails-plain.svg';
+import { ReactComponent as RaspberryPiIcon } from 'devicon/icons/raspberrypi/raspberrypi-original.svg';
 import { ReactComponent as ReactIcon } from 'devicon/icons/react/react-original.svg';
 import { ReactComponent as ReduxIcon } from 'devicon/icons/redux/redux-original.svg';
+import { ReactComponent as RubyIcon } from 'devicon/icons/ruby/ruby-original.svg';
+import { ReactComponent as RustIcon } from 'devicon/icons/rust/rust-plain.svg';
+import { ReactComponent as SassIcon } from 'devicon/icons/sass/sass-original.svg';
+import { ReactComponent as SpringIcon } from 'devicon/icons/spring/spring-original.svg';
+import { ReactComponent as SvelteIcon } from 'devicon/icons/svelte/svelte-original.svg';
+import { ReactComponent as SwiftIcon } from 'devicon/icons/swift/swift-original.svg';
+import { ReactComponent as TailwindIcon } from 'devicon/icons/tailwindcss/tailwindcss-plain.svg';
+import { ReactComponent as ThreeJSIcon } from 'devicon/icons/threejs/threejs-original.svg';
 import { ReactComponent as TypescriptIcon } from 'devicon/icons/typescript/typescript-original.svg';
+import { ReactComponent as UnityIcon } from 'devicon/icons/unity/unity-original.svg';
 import { ReactComponent as VisualStudioIcon } from 'devicon/icons/visualstudio/visualstudio-plain.svg';
 import { ReactComponent as VueJSIcon } from 'devicon/icons/vuejs/vuejs-original.svg';
-import {} from 'devicon/icons/vuejs/vuejs-original.svg';
+import { ReactComponent as WordpressIcon } from 'devicon/icons/wordpress/wordpress-original.svg';
 
 export const iconSelector = (name, props = { width: 60, height: 60 }) => {
     // Convert skill name to lowercase & remove whitespace
     const skillName = name.toLowerCase().replace(/\s/g, '');
     if (skillName.includes('aws') || skillName === 'amazonwebservices') {
         return <AWSIcon {...props} />;
+    } else if (skillName.includes('adonis')) {
+        return <AdnoisJSIcon {...props} />;
     } else if (skillName.includes('android')) {
         return <AndroidIcon {...props} />;
     } else if (skillName.includes('angular')) {
         return <AngularJSIcon {...props} />;
+    } else if (skillName.includes('arduino')) {
+        return <ArduinoIcon {...props} />;
+    } else if (skillName.includes('azure')) {
+        return <AzureIcon {...props} />;
+    } else if (skillName.includes('backbone')) {
+        return <BackboneIcon {...props} />;
     } else if (skillName.includes('bootstrap')) {
         return <BootstrapIcon {...props} />;
+    } else if (skillName.includes('clojure')) {
+        return <ClojureIcon {...props} />;
     } else if (skillName === 'c') {
         return <CIcon {...props} />;
+    } else if (skillName === 'coffeescript') {
+        return <CoffeescriptIcon {...props} />;
+    } else if (skillName.includes('couch') || skillName === 'couchdb') {
+        return <CouchDBIcon {...props} />;
     } else if (skillName === 'cplusplus' || skillName.includes('c++')) {
         return <CPlusPlusIcon {...props} />;
     } else if (skillName === 'csharp' || skillName.includes('c#') || skillName.includes('csharp')) {
@@ -52,40 +116,133 @@ export const iconSelector = (name, props = { width: 60, height: 60 }) => {
         return <D3JsIcon {...props} />;
     } else if (skillName.includes('react')) {
         return <ReactIcon {...props} />;
+    } else if (skillName.includes('dart')) {
+        return <DartIcon {...props} />;
     } else if (skillName.includes('django')) {
         return <DjangoIcon {...props} />;
     } else if (skillName.includes('docker')) {
         return <DockerIcon {...props} />;
-    } else if (skillName.includes('dotnet') || skillName.includes('.net')) {
+    } else if (
+        (skillName.includes('dotnet') || skillName.includes('.net')) &&
+        !skillName.includes('core')
+    ) {
         return <DotNetIcon {...props} />;
+    } else if (
+        (skillName.includes('dotnet') || skillName.includes('.net')) &&
+        skillName.includes('core')
+    ) {
+        return <DotNetCoreIcon {...props} />;
+    } else if (skillName.includes('drupal')) {
+        return <DrupalIcon {...props} />;
     } else if (skillName.includes('electron')) {
         return <ElectronIcon {...props} />;
+    } else if (skillName.includes('ember')) {
+        return <EmberIcon {...props} />;
+    } else if (skillName.includes('erlang')) {
+        return <ErlangIcon {...props} />;
     } else if (skillName.includes('express')) {
         return <ExpressIcon {...props} />;
+    } else if (skillName.includes('figma')) {
+        return <FigmaIcon {...props} />;
+    } else if (skillName.includes('firebase')) {
+        return <FirebaseIcon {...props} />;
+    } else if (skillName.includes('flask')) {
+        return <FlaskIcon {...props} />;
+    } else if (skillName.includes('flutter')) {
+        return <FlutterIcon {...props} />;
+    } else if (skillName.includes('fsharp') || skillName.includes('f#')) {
+        return <FSharpIcon {...props} />;
+    } else if (skillName.includes('gatsby')) {
+        return <GatsbyIcon {...props} />;
+    } else if (skillName === 'go' || skillName.includes('golang')) {
+        return <GoIcon {...props} />;
+    } else if (skillName.includes('graphql')) {
+        return <GraphQLIcon {...props} />;
+    } else if (skillName.includes('haskell')) {
+        return <HaskellIcon {...props} />;
     } else if (skillName.includes('html')) {
         return <Html5Icon {...props} />;
+    } else if (skillName.includes('jamstack')) {
+        return <JamstackIcon {...props} />;
     } else if (skillName.includes('java') && !skillName.includes('javascript')) {
         return <JavaIcon {...props} />;
     } else if (skillName.includes('javascript')) {
         return <JavascriptIcon {...props} />;
     } else if (skillName.includes('jquery')) {
         return <JQueryIcon {...props} />;
+    } else if (skillName.includes('julia')) {
+        return <JuliaIcon {...props} />;
+    } else if (skillName.includes('knockout')) {
+        return <KnockoutIcon {...props} />;
+    } else if (skillName.includes('kotlin')) {
+        return <KotlinIcon {...props} />;
+    } else if (skillName.includes('kubernetes')) {
+        return <KubernetesIcon {...props} />;
+    } else if (skillName.includes('labview')) {
+        return <LabViewIcon {...props} />;
+    } else if (skillName.includes('laravel')) {
+        return <LaravelIcon {...props} />;
+    } else if (skillName.includes('lua')) {
+        return <LuaIcon {...props} />;
+    } else if (skillName.includes('magento')) {
+        return <MagentoIcon {...props} />;
     } else if (skillName.includes('mongo')) {
         return <MongoDBIcon {...props} />;
     } else if (skillName.includes('mysql')) {
         return <MySQLIcon {...props} />;
+    } else if (skillName.includes('nest')) {
+        return <NestJSIcon {...props} />;
+    } else if (skillName.includes('next')) {
+        return <NextJSIcon {...props} />;
     } else if (skillName.includes('node')) {
         return <NodeJSIcon {...props} />;
+    } else if (skillName.includes('numpy')) {
+        return <NumpyIcon {...props} />;
+    } else if (skillName.includes('objective')) {
+        return <ObjectiveCIcon {...props} />;
+    } else if (skillName.includes('pandas')) {
+        return <PandasIcon {...props} />;
+    } else if (skillName.includes('perl')) {
+        return <PerlIcon {...props} />;
+    } else if (skillName.includes('php')) {
+        return <PHPIcon {...props} />;
+    } else if (skillName.includes('postgres')) {
+        return <PostgresIcon {...props} />;
     } else if (skillName.includes('python')) {
         return <PythonIcon {...props} />;
+    } else if (skillName === 'r' || skillName.includes('rstudio')) {
+        return <RIcon {...props} />;
+    } else if (skillName.includes('rails')) {
+        return <RailsIcon {...props} />;
+    } else if (skillName.includes('raspberrypi')) {
+        return <RaspberryPiIcon {...props} />;
     } else if (skillName.includes('redux')) {
         return <ReduxIcon {...props} />;
+    } else if (skillName.includes('ruby')) {
+        return <RubyIcon {...props} />;
+    } else if (skillName.includes('rust')) {
+        return <RustIcon {...props} />;
+    } else if (skillName.includes('sass') || skillName.includes('scss')) {
+        return <SassIcon {...props} />;
+    } else if (skillName.includes('spring')) {
+        return <SpringIcon {...props} />;
+    } else if (skillName.includes('svelte')) {
+        return <SvelteIcon {...props} />;
+    } else if (skillName.includes('swift')) {
+        return <SwiftIcon {...props} />;
+    } else if (skillName.includes('tailwind')) {
+        return <TailwindIcon {...props} />;
+    } else if (skillName.includes('three')) {
+        return <ThreeJSIcon {...props} />;
     } else if (skillName.includes('typescript')) {
         return <TypescriptIcon {...props} />;
     } else if (skillName.includes('visualstudio')) {
         return <VisualStudioIcon {...props} />;
     } else if (skillName.includes('vue')) {
         return <VueJSIcon {...props} />;
+    } else if (skillName.includes('wordpress')) {
+        return <WordpressIcon {...props} />;
     }
+
     return <div>{name}</div>;
 };
